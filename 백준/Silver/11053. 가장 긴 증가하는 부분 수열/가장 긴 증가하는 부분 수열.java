0afcb64ae3fe5763c886721c1b0cfbd2312f6c12
Yeeyahou <jungwon2580147@gmail.com>
2025-04-12 03:55:31 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 124 ms, Memory: 14788 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,46 +1,40 @@
 import java.util.*;
 import java.io.*;
- 
+
 public class Main {
-	
-	static int[] seq;
-	static int[] dp;
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		seq = new int[N];
-		dp = new int[N];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    static int[] dp;
+    static int[] A;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        dp = new int[N];
+        A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
-            seq[i] = Integer.parseInt(st.nextToken());
-		}
-		int res = -1;
-		// 0 ~ N-1 까지 모든 부분수열 탐색 
-		for(int i = 0; i < N; i++) {
-			res = Math.max(LIS(i),res);
-		}
-		
-		System.out.println(res);
-	}
-	
-	
-	static int LIS(int N) {
-		
-		// 만약 탐색하지 않던 위치의 경우 
-		if(dp[N] == 0) {
-			dp[N] = 1;	// 1로 초기화 
-			
-			// N-1 부터 0까지중 N보다 작은 값들을 찾으면서 재귀호출. 
-			for(int i = N - 1; i >= 0; i--) {
-				if(seq[i] < seq[N]) {
-					dp[N] = Math.max(dp[N], LIS(i) + 1);
-				}
-			}
-		}
-		return dp[N];
-	}
+            A[i] = Integer.parseInt(st.nextToken());
+
+        }
+        int res = -1;
+        for (int i = 0; i < N; i++) {
+            res = Math.max(solve(i), res);
+        }
+        System.out.println(res);
+    }
+
+    static int solve(int n) {
+        if (dp[n] == 0) {
+            dp[n] = 1;
+            for (int i = n - 1; i >= 0; i--) {
+                if (A[i] < A[n]) {
+                    dp[n] = Math.max(dp[n], solve(i) + 1);
+                }
+            }
+        }
+        return dp[n];
+    }
+
 }
