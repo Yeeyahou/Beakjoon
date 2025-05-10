86ca32ffee908062261d95924dd68fc9094c1d22
Yeeyahou <jungwon2580147@gmail.com>
2025-05-10 21:56:54 +0900

    [Silver I] Title: 아보와 킨텍스, Time: 136 ms, Memory: 17368 KB -BaekjoonHub

--- 백준/Silver/32206. 아보와 킨텍스/아보와 킨텍스.java
+++ 백준/Silver/32206. 아보와 킨텍스/아보와 킨텍스.java
@@ -4,18 +4,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
         String str = br.readLine();
-        char prev = ' ';
-        int consNum = 0;
-        for (char c : str.toCharArray()) {
-            if (c == prev)
-                consNum++;
-            else
-                prev = c;
-        }
-
+        
         int res = 25 * (n + 1) + 1;
         System.out.println(res);
     }
