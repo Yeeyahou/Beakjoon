87c1b5b46b92a2980d2c70d5d756a73aa1c1dff3
Yeeyahou <jungwon2580147@gmail.com>
2025-05-25 21:27:14 +0900

    [Silver I] Title: 오르막 수, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.java
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -2,24 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[][] dp;
+    static int[] dp = new int[11];
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = sToi(br.readLine());
-        dp = new int[n + 1][11];
 
-        for (int i = 0; i <= 10; i++) {
-            dp[0][i] = 1;
+        for (int i = 1; i <= 10; i++) {
+            dp[i] = 1;
         }
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= 10; j++) {
-                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 10007;
+                dp[j] = (dp[j] + dp[j-1]) % 10007;
             }
         }
 
-        System.out.println(dp[n][10]);
+        System.out.println(dp[10]);
     }
 
     static int sToi(String s) {
