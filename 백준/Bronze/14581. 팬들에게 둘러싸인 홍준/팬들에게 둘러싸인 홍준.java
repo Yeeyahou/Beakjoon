7a22fecb5bc8a9286147f4a1a54925cdfc8191b1
Yeeyahou <jungwon2580147@gmail.com>
2025-06-18 01:38:50 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 184 ms, Memory: 18268 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.nextLine(); // 홍준의 아이디 입력
+
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + id + "::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
