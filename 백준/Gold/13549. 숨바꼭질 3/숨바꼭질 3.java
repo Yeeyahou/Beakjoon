4e2582955a128b28c9aa83a735dbb688ad624bb2
Byun JungWon <2271187@hansung.ac.kr>
2025-11-18 01:52:51 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 1212 ms, Memory: 263156 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,8 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
