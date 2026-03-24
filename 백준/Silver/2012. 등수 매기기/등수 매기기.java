d4b37d2b56b7515cac54383d92e33787251a5951
Byun JungWon <2271187@hansung.ac.kr>
2026-03-24 14:11:44 +0900

    [Silver III] Title: 등수 매기기, Time: 772 ms, Memory: 51856 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        long res = 0;
+
+        for (int i = 0; i < n; i++) {
+            res += Math.abs((i + 1) - arr[i]);
+        }
+        System.out.println(res);
+    }
+}
